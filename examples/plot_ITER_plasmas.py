edbd4e36ee7e0ea8337c14979de49869ac43d282
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-07-14 10:29:53 +0200

    update colorbars

--- examples/plot_ITER_plasmas.py
+++ examples/plot_ITER_plasmas.py
@@ -5,16 +5,23 @@
 # https://doi.org/10.1063/1.3328818
 
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+
 import numpy as np
-from plasma_boundaries import parameters, compute_psi
+import plasma_boundaries
 
 # plasma parameters
-params = parameters.ITER
+params = plasma_boundaries.ITER
 
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True, figsize=(10, 4.8))
-for ax, config in zip([ax1, ax2, ax3], ["non-null", "single-null", "double-null"]):
+
+
+for ax, config in zip(
+    [ax1, ax2, ax3],
+    ["non-null", "single-null", "double-null"]):
     # compute psi
-    psi = compute_psi(params, config=config)
+    psi = plasma_boundaries.compute_psi(params, config=config)
 
     # plot the results
     xmin, xmax = 0.6, 1.35
@@ -25,13 +32,27 @@ for ax, config in zip([ax1, ax2, ax3], ["non-null", "single-null", "double-null"
     X, Y = np.meshgrid(x, y)
     Z = psi(X, Y)  # compute magnetic flux
 
-    # ax.title("ITER single-null")
-    levels = np.linspace(Z.min(), 0, num=25)
-    CS = ax.contourf(X, Y, Z, levels=levels, vmax=0)
-    ax.contour(X, Y, Z, levels=[0], colors="black")
+    # add filled contours
+    levels2 = np.unique(np.linspace(Z.min(), -Z.min(), num=100))
+    norm = mcolors.TwoSlopeNorm(vmin=Z.min(), vcenter=0., vmax=-Z.min())
+    CSF = ax.contourf(
+        X, Y, Z, levels=levels2, cmap="coolwarm", norm=norm,
+        vmax=-Z.min(), extend="max")
+
+    # add contours
+    levels = np.unique(
+        np.append(
+            np.linspace(Z.min(), 0, num=10), np.linspace(0, Z.max(), num=20)))
+    CS = ax.contour(
+        X, Y, Z, levels=levels[levels != 0], colors="black",
+        linestyles="solid")
+    separatrix = ax.contour(
+        X, Y, Z, levels=[0], colors="black", linestyles="dashed")
+    ax.clabel(separatrix, inline=True, fmt=r"$\Psi = $%.0f")
+
     ax.set_title('ITER ' + config)
     ax.set_xlabel('Radius $R/R_0$')
     ax.set_aspect("equal")
 ax1.set_ylabel('Height $Z/R_0$')
-fig.colorbar(CS, label="Magnetic flux $\Psi$", format="%.3f")
+plt.colorbar(CSF, label="Magnetic flux $\Psi$", format="%.3f")
 plt.show()
