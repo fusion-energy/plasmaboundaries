00147f951f935295e2197281fd38d81f4a8a966e
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-08-04 16:35:30 +0200

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="plasmaboundaries",
-    version="0.0.3",
+    version="0.0.4",
     author="Remi Delaporte-Mathurin",
     author_email="rdelaportemathurin@gmail.com",
     description="Determine plasma flux functions for various plasma parameters and plasma configurations",
