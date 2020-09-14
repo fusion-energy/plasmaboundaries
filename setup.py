a41eca9357cf6fccdc0343c1d92d1fd59223303a
remithewarrior <delaporte.mathurin@laposte.net>
2020-09-14 14:32:19 +0200

    added package data

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@ import setuptools
 
 setuptools.setup(
     name="plasmaboundaries",
-    version="0.1.0",
+    version="0.1.1",
     author="Remi Delaporte-Mathurin",
     author_email="rdelaportemathurin@gmail.com",
     description="Determine plasma flux functions for various plasma parameters and plasma configurations",
     url="https://github.com/RemiTheWarrior/plasma-boundaries",
     packages=setuptools.find_packages(),
+    package_dir={"plasmaboundaries": "plasmaboundaries"},
+    package_data={
+        "plasmaboundaries": [
+            "requirements.txt",
+            "README.md",
+            "LICENSE.txt",
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
