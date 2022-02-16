83c516c7efba66f6ac6786169f16c316bb9500a5
Jonathan <mail@jshimwell.com>
2022-02-16 22:35:49 +0000

    restructured package and install

--- setup.py
+++ setup.py
@@ -1,25 +1,4 @@
 import setuptools
 
-setuptools.setup(
-    name="plasmaboundaries",
-    version="0.1.8",
-    author="Remi Delaporte-Mathurin",
-    author_email="rdelaportemathurin@gmail.com",
-    description="Determine plasma flux functions for various plasma parameters and plasma configurations",
-    url="https://github.com/RemiTheWarrior/plasma-boundaries",
-    packages=setuptools.find_packages(),
-    package_dir={"plasmaboundaries": "plasmaboundaries"},
-    data_files=[("", ["LICENSE.txt"])],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        'matplotlib',
-        'scipy',
-        'numpy',
-        'sympy',
-    ],
-    zip_safe=True,
-)
+if __name__ == "__main__":
+    setuptools.setup()
